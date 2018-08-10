f905a299a8e857a07991699a72869712e561fa66
yangxi <599321378@qq.com>
2018-08-10 18:52:45 +0800

    feat:重新打包

--- build/webpack.build.js
+++ build/webpack.build.js
@@ -12,7 +12,7 @@ Object.assign(config.output, {
 	filename: './[name].[chunkhash].js',
 	chunkFilename: './[id].[chunkhash].js',
 	publicPath: '',
-	path: path.resolve(__dirname, '../docs')
+	path: path.resolve(__dirname, '../assets')
 })
 
 config.module.rules = config.module.rules.concat([
@@ -47,41 +47,30 @@ config.plugins = (config.plugins || []).concat([
 	//想看包文件的情况，可以打开
 	//new BundleAnalyzerPlugin(),
 	new ExtractTextPlugin('./[name].[chunkhash].css'),
-	// new CopyWebpackPlugin([{
-	// 	from: 'src/static'
-	// }]),
+	new CopyWebpackPlugin([{
+		from: 'src/static'
+	}]),
 	
 	new HtmlWebpackPlugin({
-		filename: '../docs/index.html',
+		filename: '../assets/index.html',
 		template: 'src/index.html'
-	})
+	}),
+	
 ])
 
-fs.remove(path.resolve(__dirname, '../docs'))
-console.log('文件夹docs已删除')
-fs.remove(path.resolve(__dirname, '../index.html'))
-console.log('index.html已删除')
+fs.remove(path.resolve(__dirname, '../assets'))
+console.log('文件夹assets已删除')
+fs.remove(path.resolve(__dirname, '../docs'));
+console.log('文件夹docs已删除');
 
 console.log('正在打包')
 var compiler = webpack(config, (err, stats) => {
 	console.log(err)
 	console.log('打包成功')
 	console.log('[webpack]', stats.toString({}))
-	updateHtml()
+	fs.copy(path.resolve(__dirname, '../assets'), path.resolve(__dirname, '../docs'), function () {
+		console.log('已复制assets到docs');
+	});
 })
 
-function updateHtml () {
-	let filePathFrom = path.join(__dirname, '../docs/index.html')
-	let filePathTo = path.join(__dirname, '../index.html')
-	fs.readFile(filePathFrom, (err, data) => {
-		if (err) throw err
-		let html = data.toString()
-		html = html.replace('src="./app.', 'src="./docs/app.')
-		html = html.replace('href="./app.', 'href="./docs/app.')
-		fs.writeFile(filePathTo, html, (err) => {
-			if (err) throw err
-			console.log('index.html生成成功！')
-		})
-	})
-}
 
